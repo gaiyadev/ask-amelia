6f29aaa483718cf71b70d0951402e316430e6e52
Gaiya M Obed <gaiyaobed94@gmail.com>
2020-06-21 18:01:33 +0100

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+  <div id="app" class="container-fluid">
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
